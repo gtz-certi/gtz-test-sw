f8953d2e010b59e035904b82a88222b15f32b3d1
Gustavo Tomin Zavadski <gtz@certi.org.br>
2020-05-06 13:35:06 -0300

    Add function to return the formatted number

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,72 @@
 const http = require('http');
 
-port = 3000;
+const port = 3000;	
+
+const firstTwenty = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",
+                     "dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"];
+const dozens = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"];
+const hundreds = ["cem", "duzentos", "trezentos", "quatrocentos", "quinhetos", "seiscentos", "setecentos", "oitocentos", "novecentos"];
+
+function converterNumberFormat(a) {
+  var numberConverted = "";
+  var number = a; 
+  var aux;
+
+  if (number < 0) {
+    number = -number;
+    numberConverted += "menos "; 
+  }
+
+  if (number != 0) {
+    if (number >= 1000) {
+      aux = parseInt(number / 1000);
+      if (aux >= 20) {
+        numberConverted += dozens[parseInt(aux / 10) - 2];
+        aux = aux % 10;
+        if (aux > 0) {
+          numberConverted += " e ";
+          numberConverted += firstTwenty[aux];  
+        }
+      } else if (aux > 0) {
+        numberConverted += firstTwenty[aux];
+      }  
+      numberConverted += " mil";
+    }
+
+    aux = parseInt(number % 1000);
+    if (aux >= 100) {
+      if (numberConverted != "" && numberConverted != "menos ") {
+        numberConverted += " e ";
+      }
+      numberConverted += hundreds[parseInt(aux / 100) - 1];
+    }
+
+    aux = parseInt(number % 100);
+    if (aux > 0) {
+      if (numberConverted != "" && numberConverted != "menos ") {
+        numberConverted += " e ";
+      }
+      if (aux >= 20) {
+        numberConverted += dozens[parseInt(aux / 10) - 2];
+        aux = aux % 10;
+        if (aux > 0) {
+          numberConverted += " e ";
+          numberConverted += firstTwenty[aux];  
+        }
+      } else if (aux > 0) {
+        numberConverted += firstTwenty[aux];
+      }  
+    }
+  } else {
+    numberConverted += firstTwenty[number];
+  }
+
+  return numberConverted;
+}
+
+function isFloat(n){
+  return (Number(n) == n) && ((n % 1) !== 0);
+}
 
 const requestHandler = function (req, res) {
   var code = 200;
@@ -8,31 +74,42 @@ const requestHandler = function (req, res) {
   var url = req.url;
   url = url.substr(1);
   try {
-    if (req.method !== 'GET') throw "method";
+    if (req.method !== "GET") throw "method";
     if (url == '') throw "empty";
     if (isNaN(url)) throw "NaN";
+    if (isFloat(url)) throw "float";
     if ((url < -99999) || (99999 < url)) throw "outOfBounds";
   }
   catch (err){
     switch (err) {
-      case 'method':
+      case "method":
         code = 404;
-        string = 'The method is not allowed!\n';
+        string = "Esse método não é permitido!\n";
         break;
-      case 'empty':
+      case "empty":
         code = 400;
-        string = 'The url is empty!\n';
+        string = "A URL está vazia!\n";
         break;
-      case 'NaN':
+      case "NaN":
         code = 400;
-        string = 'The url is not a number!\n';
+        string = "A URL não é um número!\n";
+        break;
+      case "float":
+        code = 400;
+        string = "O número é do tipo real!\n";
         break;
       case 'outOfBounds':
         code = 400;
-        string = 'The number is out of bounds!\n';
+        string = "O número está fora dos limites!\n";
         break;
     }
   }
+
+  if (code == 200) {
+    var cardinal = converterNumberFormat(url)
+    string = '{"extenso":"'+ cardinal +'"}';
+  }
+
   res.writeHead(code, {'Content-Type': 'text/html'});
   res.end(string);
 }
